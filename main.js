bef152147bd529e352d63212c9708b8ed93d4af4
janghjun <yyuyiy@naver.com>
2025-01-26 01:13:12 +0900

    계산기  JS

---
+++ main.js
@@ -0,0 +1,80 @@
+const currDisplay = document.querySelector(".curr-display");
+const prevDisplay = document.querySelector(".prev-display");
+const numbers = document.querySelectorAll(".number");
+const operands = document.querySelectorAll(".operation");
+const clearBtn = document.querySelector(".clear");
+const delBtn = document.querySelector(".delete");
+const equalBtn = document.querySelector(".equal");
+let operation = null;
+
+function appendNumber(number) {
+    if (number === "." && currDisplay.innerText.includes(".")) return;
+    currDisplay.innerText += number;
+}
+
+function chooseOperation(operand) {
+    if (currDisplay.innerText === "") return;
+    if (prevDisplay.innerText !== "") compute();
+    operation = operand;
+    prevDisplay.innerText = currDisplay.innerText + " " + operation;
+    currDisplay.innerText = "";
+}
+
+function clearDisplay() {
+    currDisplay.innerText = "";
+    prevDisplay.innerText = "";
+    operation = null;
+}
+
+function compute() {
+    const previousValue = parseFloat(prevDisplay.innerText);
+    const currentValue = parseFloat(currDisplay.innerText);
+
+    if (isNaN(previousValue) || isNaN(currentValue) || !operation) return;
+
+    let result;
+    switch (operation) {
+        case "+":
+            result = previousValue + currentValue;
+            break;
+        case "-":
+            result = previousValue - currentValue;
+            break;
+        case "*":
+            result = previousValue * currentValue;
+            break;
+        case "/":
+            result = previousValue / currentValue;
+            break;
+        default:
+            return;
+    }
+
+    currDisplay.innerText = result;
+    prevDisplay.innerText = "";
+    operation = null;
+}
+
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+        appendNumber(number.innerText);
+    });
+});
+
+operands.forEach((operand) => {
+    operand.addEventListener("click", () => {
+        chooseOperation(operand.innerText);
+    });
+});
+
+clearBtn.addEventListener("click", () => {
+    clearDisplay();
+});
+
+equalBtn.addEventListener("click", () => {
+    compute();
+});
+
+delBtn.addEventListener("click", () => {
+    currDisplay.innerText = currDisplay.innerText.slice(0, -1);
+});
